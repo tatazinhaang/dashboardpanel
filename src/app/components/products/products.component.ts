638b334097c83485ccdddf4d53cbde286e0695a7
Angela Talyta <tatazinhagms2@gmail.com>
2024-04-11 15:51:30 -0300

    Adding add, remove, and update functions to the product component

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-products',
@@ -10,12 +11,9 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angul
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
-
   productForm!:FormGroup;
 
-
   ngOnInit():void {
-
     this.setForm();
   }
 
@@ -30,10 +28,45 @@ export class ProductsComponent {
 
   productList:any=[];
   submit() {
-    console.log(this.productForm.value);
-
     this.productList.push(this.productForm.value);
-
-    console.log("Product List: ", this.productList);
+    this.closeModal('addProductModal');
   }
+
+  index:any
+  openModal(modalId:any, index:any) {
+    if(modalId == 'updateProductModal') {
+      this.productForm = new FormGroup ({
+      productId: new FormControl(this.productList[index].productId),
+      productName: new FormControl(this.productList[index].productName),
+      productQuantity: new FormControl(this.productList[index].productQuantity),
+      productPrice: new FormControl(this.productList[index].productPrice),
+    })
+
+    }
+    this.index=index;
+    const modalElement:any = document.getElementById(modalId);
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
+  }
+
+  closeModal(modalId:any) {
+    const modalElement:any = document.getElementById(modalId);
+    const modal = bootstrap.Modal.getInstance(modalElement);
+    if (modal) {
+      modal.hide();
+    }
+  }
+
+  delete() {
+    this.productList.splice(this.index, 1);
+    this.closeModal('deleteModal');
+  }
+
+  update() {
+    this.productList[this.index].productId = this.productForm.value.productId;
+    this.productList[this.index].productIName = this.productForm.value.productName;
+    this.productList[this.index].productQuantity = this.productForm.value.productQuantity;
+    this.productList[this.index].productPrice = this.productForm.value.productPrice;
+  }
+
 }

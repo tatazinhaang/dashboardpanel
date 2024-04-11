016b2e8177a7744b4879a048e87e1097806d670a
Angela Talyta <tatazinhagms2@gmail.com>
2024-04-11 06:33:38 -0300

    Adding features to the product component

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
 
+  productForm!:FormGroup;
+
+
+  ngOnInit():void {
+
+    this.setForm();
+  }
+
+  setForm() {
+    this.productForm = new FormGroup ({
+      productId: new FormControl(''),
+      productName: new FormControl(''),
+      productQuantity: new FormControl(''),
+      productPrice: new FormControl(''),
+    })
+  }
+
+  productList:any=[];
+  submit() {
+    console.log(this.productForm.value);
+
+    this.productList.push(this.productForm.value);
+
+    console.log("Product List: ", this.productList);
+  }
 }
